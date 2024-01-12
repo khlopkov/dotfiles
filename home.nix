d9b530872f595faa891796ce444c5a6d7a5ef5fc
ETSilence <khlopkovdd@gmail.com>
2024-01-12 20:03:16 +0100

    feat: moved .Xresources to a dotfiles

--- home.nix
+++ home.nix
@@ -21,17 +21,9 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
     ".config/nvim".source = ./dotfiles/nvim;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".Xresources".source = ./dotfiles/Xresources;
+    ".xinitrc".source = ./dotfiles/xinitrc;
   };
 
   # Home Manager can also manage your environment variables through
@@ -69,7 +61,5 @@
   };
 
   fonts.fontconfig.enable = true;
-  xresources.properties = import ./Xresourses.nix;
-
 }
 
