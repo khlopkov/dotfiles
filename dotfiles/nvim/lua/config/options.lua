38644932c329018f1fde3cd777f8b41ae2dffd4c
ETSilence <khlopkovdd@gmail.com>
2024-01-14 02:12:54 +0100

    set nvim option to show current line

--- dotfiles/nvim/lua/config/options.lua
+++ dotfiles/nvim/lua/config/options.lua
@@ -4,6 +4,7 @@ vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
 local opt = vim.opt
 
 opt.relativenumber = true -- add relative line numbers
+vim.wo.number = true -- Show focused line nubmer
 opt.expandtab = true -- Use spaces instead of tabs
 opt.cursorline = true -- Enable highlighting of the current line
 opt.termguicolors = true -- enable termguicolors
