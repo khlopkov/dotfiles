35337e7503e3b20d9c6f99d9ed5bb6d0008003ca
dmitrii.khlopkov <dmitrii.khlopkov@semrush.com>
2024-03-14 19:08:39 +0100

    update java devkit

--- modules/devkit/java.nix
+++ modules/devkit/java.nix
@@ -2,15 +2,22 @@
 let
   # See compatibility matrix
   # https://docs.gradle.org/current/userguide/compatibility.html
-  jdk = pkgs.jdk20;
+  jdk = pkgs.jdk21;
+
   gradle = (pkgs.callPackage pkgs.gradle-packages.gradle_8 {
     # Reuse same JDK version
     java = jdk;
   });
+
+  maven = pkgs.maven.override {
+    jdk = jdk;
+  };
 in {
   home.packages = [
     jdk # Java DevKit 21
     gradle # Gradle build tool for Java projects
+    maven
   ];
 
+  home.sessionVariables = { JAVA_HOME = "${jdk}"; };
 }
