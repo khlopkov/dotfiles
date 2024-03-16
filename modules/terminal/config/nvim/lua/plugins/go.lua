57003e796104ee732c9b0624fd8d197752c02d7e
dmitrii.khlopkov <dmitrii.khlopkov@semrush.com>
2024-03-16 17:25:03 +0100

    add golangci-lint

--- modules/terminal/config/nvim/lua/plugins/go.lua
+++ modules/terminal/config/nvim/lua/plugins/go.lua
@@ -13,4 +13,20 @@ return {
     ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      optional = true,
+      linters_by_ft = {
+        go = { "golangcilint" },
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "golangci-lint" })
+    end,
+  },
 }
