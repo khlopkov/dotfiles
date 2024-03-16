06cc2fccabc7296d88e6b6b459ac5803288432a6
ETSilence <khlopkovdd@gmail.com>
2024-03-16 19:13:09 +0100

    enable tabnine

--- modules/terminal/config/nvim/lua/config/extras.lua
+++ modules/terminal/config/nvim/lua/config/extras.lua
@@ -3,4 +3,5 @@ return {
   { import = "lazyvim.plugins.extras.lang.helm" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
   { import = "lazyvim.plugins.extras.lang.java" },
+  { import = "lazyvim.plugins.extras.coding.tabnine" },
 }
