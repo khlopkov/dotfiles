56219cd6bd13fde95fc4a15a7d901433b96545f8
dmitrii.khlopkov <dmitrii.khlopkov@semrush.com>
2025-08-09 12:56:09 +0200

    chore: upgrade packages

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixgl = {
       url = "github:guibou/nixGL";
       # Probably better to remove to work with pinned nixpkgs from of nixGL's inputs.
@@ -11,7 +11,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     phps.url = "github:fossar/nix-phps";
